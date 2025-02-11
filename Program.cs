159486ef625d75506e095c282a27358f3f22a24a
ruben <caibugatti.ruben@franchettisalviani.net>
2025-02-11 13:09:12 +0100

    es1
    funziona

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num(string numero)
+{
+    int s = 0,cont=numero.Length-1;
+    for (int i = 0; i < numero.Length; i++)
+    {
+        if (numero[i] == '1')
+        {
+            s +=(int) Math.Pow(2,cont);
+        }
+        cont--;
+    }
+    return s;
+}
+string numero = "";
+Console.WriteLine("dimmi il numero binario(max lung 8, se superi 8 cifre, le cifre eccessive verranno eliminate)");
+numero = Console.ReadLine();
+numero = numero.Trim();
+numero = numero.Substring(0,Math.Min(8,numero.Length));
+int s = num(numero);
+Console.WriteLine(s);
