fcab64251b3171943d24ffe54f552fa0a23811cb
ruben <caibugatti.ruben@franchettisalviani.net>
2025-02-11 13:22:36 +0100

    es2
    torna

--- Program.cs
+++ Program.cs
@@ -11,6 +11,18 @@
     }
     return s;
 }
+string fras(string[] frase)
+{
+    string frase1 = "";
+    for(int i = 0; i < frase.Length; i++)
+    {
+        if (frase[i]!="")
+        {
+            frase1 += frase[i]+" ";
+        }  
+    }
+    return frase1;
+}
 string numero = "";
 Console.WriteLine("dimmi il numero binario(max lung 8, se superi 8 cifre, le cifre eccessive verranno eliminate)");
 numero = Console.ReadLine();
@@ -18,3 +30,8 @@ numero = numero.Trim();
 numero = numero.Substring(0,Math.Min(8,numero.Length));
 int s = num(numero);
 Console.WriteLine(s);
+Console.WriteLine("dimmi la frase da aggiustare");
+string frase = Console.ReadLine();
+frase = frase.Trim();
+string[] frase1 = frase.Split(" ");
+Console.WriteLine(fras(frase1));
