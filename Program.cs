8b60f5fdab7c24b59f73f12290c1d982d762e988
ruben <caibugatti.ruben@franchettisalviani.net>
2025-02-12 08:52:55 +0100

    es 4
    funziona

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿int num(string numero)
+﻿using System.Security.Cryptography;
+
+int num(string numero)
 {
     int s = 0,cont=numero.Length-1;
     for (int i = 0; i < numero.Length; i++)
@@ -28,6 +30,24 @@ void SpezzaMeta(string s)
     Console.WriteLine(s.Substring(0,s.Length/2));
     Console.WriteLine(s.Substring(s.Length / 2));
 }
+bool palindroma(string s)
+{
+    string VF = s;
+    int p = VF.Length-1 ;
+    for(int i = 0; i < VF.Length; i++)
+    {
+        if (s[i] != VF[p])
+        {
+            return false;
+        }
+        if (i > s.Length / 2)
+        {
+            return true;
+        }
+        p--;
+    }
+    return false;
+}
 string numero = "";
 Console.WriteLine("dimmi il numero binario(max lung 8, se superi 8 cifre, le cifre eccessive verranno eliminate)");
 numero = Console.ReadLine();
@@ -44,3 +64,6 @@ Console.WriteLine("dimmi la frase da spezzare");
 string ok = Console.ReadLine();
 ok = ok.Trim();
 SpezzaMeta(ok);
+Console.WriteLine("dammi la parola per verificare se è palindroma");
+string stringa=Console.ReadLine();
+Console.WriteLine(palindroma(stringa));
